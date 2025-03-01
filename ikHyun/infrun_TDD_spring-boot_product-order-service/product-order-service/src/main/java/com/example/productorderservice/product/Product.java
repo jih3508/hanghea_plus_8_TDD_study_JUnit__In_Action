c3461cda19419382007b48fd77eca6653852b138
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2025-03-02 01:01:29 +0900

    강의명 : 실전! 스프링부트 상품-주문 API 개발로 알아보는 TDD 색션2.상품 등록 API 개발

---
+++ ikHyun/infrun_TDD_spring-boot_product-order-service/product-order-service/src/main/java/com/example/productorderservice/product/Product.java
@@ -0,0 +1,36 @@
+package com.example.productorderservice.product;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private int price;
+
+    private DiscountPolicy discountPolicy;
+
+    Product(final String name, final int price, final DiscountPolicy discountPolicy) {
+        Assert.hasText(name, "상품명은 필수입니다.");
+        Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+        Assert.notNull(discountPolicy, "할인 정책은 필수 입니다.");
+        this.name = name;
+        this.price = price;
+        this.discountPolicy = discountPolicy;
+
+
+    }
+
+}
