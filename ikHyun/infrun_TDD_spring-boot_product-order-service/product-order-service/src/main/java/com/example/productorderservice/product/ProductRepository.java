13522744834117fb313e93cce275681e506d69a8
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2025-03-02 02:03:15 +0900

    상품 등록 API 개발 README 추가

--- ikHyun/infrun_TDD_spring-boot_product-order-service/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ ikHyun/infrun_TDD_spring-boot_product-order-service/product-order-service/src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -2,7 +2,5 @@ package com.example.productorderservice.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
