2f273f2412da44eea3439c85911d04df059e29e2
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2025-03-16 03:27:28 +0900

    색션 6. 주문 결제 API 개발 JPA 적용하기

--- ikHyun/infrun_TDD_spring-boot_product-order-service/product-order-service/src/test/java/com/example/productorderservice/order/OrderApiTest.java
+++ ikHyun/infrun_TDD_spring-boot_product-order-service/product-order-service/src/test/java/com/example/productorderservice/order/OrderApiTest.java
@@ -13,7 +13,10 @@ public class OrderApiTest extends ApiTest {
     void 상품주문(){
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
 
-        final var response = OrderSteps.상품주문요청(OrderSteps.상품주문요청_생성());
+        final var request = OrderSteps.상품주문요청_생성();
+
+        final var response = OrderSteps.상품주문요청(request);
+
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
     }
